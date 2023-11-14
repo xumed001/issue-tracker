bf71f46f0aece1ac0529efc29183519f65d01811
Sumed Tamang <xumed001@gmail.com>
2023-11-14 11:50:51 -0600

    added style to Navbar links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,18 @@
-import Link from 'next/link'
-import React from 'react'
-import { AiFillBug } from 'react-icons/ai'
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import { AiFillBug } from 'react-icons/ai';
+import classNames from 'classnames';
 
 const NavBar = () => {
+   const currentPath = usePathname();
+
    const links = [
         { lable: 'Dashboard', href: '/'},
         { lable: 'Issues', href: '/issues'},
    ] 
-
 
   return (
     <nav className='flex items-center space-x-6 border-b mb-5 px-5 h-14'>
@@ -16,9 +21,12 @@ const NavBar = () => {
             {links.map(link => 
                 <Link 
                     key={link.href} 
-                    className='text-zinc-500 hover:text-zinc-800 transition-colors' 
-                    href={link.href}
-                    >{link.lable}
+                    className={classNames({
+                        'text-zinc-900': link.href === currentPath,
+                        'text-zinc-500': link.href !== currentPath,
+                        'hover:text-zinc-800 transition-colors': true 
+                    })}
+                    href={link.href}>{link.lable}
                 </Link>)}
         </ul>
     </nav>
